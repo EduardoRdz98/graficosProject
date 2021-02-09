18cfe4795186cb35b52e1a2a760d0869c674709a
Tatsuyon <37230324+Tatsuyon@users.noreply.github.com>
2021-02-08 20:48:39 -0700

    Buttons displayed

--- src/com/codebind/MainMenu.java
+++ src/com/codebind/MainMenu.java
@@ -1,12 +1,30 @@
 package com.codebind;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MainMenu {
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private JButton button6;
+public class MainMenu extends JFrame {
+    private JButton a1Button;
+    private JButton a2Button;
+    private JButton a3Button;
+    private JButton a4Button;
+    private JButton a5Button;
+    private JButton a6Button;
+    private JPanel jPanel;
+    private JPanel mainpanel;
+
+    public MainMenu() {
+        setTitle("Main menu");
+        setSize(400, 500);
+
+        add(mainpanel);
+
+        a1Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(null, "Hello world");
+            }
+        });
+    }
 }
---
+++ src/com/codebind/main/Main.java
@@ -0,0 +1,17 @@
+package com.codebind.main;
+
+import com.codebind.MainMenu;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        MainMenu menu = new MainMenu();
+        menu.setVisible(true);
+//        JFrame frame = new JFrame("MainMenu");
+//        frame.setContentPane(new MainMenu().jPanel);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.pack();
+//        frame.setVisible(true);
+    }
+}
